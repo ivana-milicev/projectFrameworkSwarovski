d5192a99654cfe336f65c8b7a7554ce3fd3a4910
Ivana Milicev <ivana.milicev0708@gmail.com>
2025-09-23 13:47:36 +0200

    Add new LogInTest and update LogInPage

--- src/main/java/pages/LogInPage.java
+++ src/main/java/pages/LogInPage.java
@@ -7,10 +7,37 @@ import java.time.Duration;
 
 public class LogInPage extends BasePage {
 
-    public By emailInputFieldLocator = By.id("sacLoginForm-j_username");
+//    Locators:
 
+    public By emailInputFieldLocator = By.id("id=\"sacLoginForm-j_username\"");
+    public By errorMessageLocator = By.xpath("//*[text()=\"Your email address or password was incorrect.\"]");
+    public By continueButtonLocator = By.xpath("//*[text()=\"Continue\"]");
+    public By passwordInputFieldLocator = By.id("sacLoginForm-j_password");
+    public By loginButtonLocator = By.xpath("//*[text()=\"Login\"]");
+    public By myAccountPageHeadingLocator = By.className("h1");
+    public By registerPageHeadingLocator = By.xpath("//*[text()=\"Register\"]");
+
+//    Constructor:
 
     public LogInPage(WebDriver driver, Duration timeout) {
         super(driver, timeout);
     }
+
+//    Actions:
+
+    public void loginFunctionality (String emailInput, String passwordInput) {
+        driver.findElement(emailInputFieldLocator).sendKeys(emailInput);
+        driver.findElement(continueButtonLocator).click();
+        driver.findElement(passwordInputFieldLocator).sendKeys(passwordInput);
+        driver.findElement(loginButtonLocator).click();
+    }
+
+    public String myAccountPageCheck () {
+        return driver.findElement(myAccountPageHeadingLocator).getText();
+    }
+
+    public String errorMessageAppearance () {
+        return driver.findElement(errorMessageLocator).getText();
+    }
+
 }
---
+++ src/test/java/appTests/LogInTest.java
@@ -0,0 +1,49 @@
+package appTests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import pages.HomePage;
+import pages.LogInPage;
+
+import java.time.Duration;
+
+public class LogInTest extends BaseTest {
+
+//    Locators:
+
+    HomePage homePage = new HomePage(driver, Duration.ofSeconds(20));
+    LogInPage logInPage = new LogInPage(driver, Duration.ofSeconds(20));
+
+//    Tests:
+
+    @Test
+    public void validLogInTest() {
+        homePage.clickOnLogInButton();
+        logInPage.loginFunctionality("ivanans90@hotmail.com", "swarovskiPass456");
+
+        String headingText = logInPage.myAccountPageCheck();
+        System.out.println("My Account page heading text: " + headingText);
+
+        Assert.assertTrue("Result heading should contain 'My Account'", headingText.contains("My Account"));
+    }
+
+    @Test
+    public void testInvalidEmail() {
+        homePage.clickOnLogInButton();
+        logInPage.loginFunctionality("ivanans@hotmail.com", "swarovskiPass456");
+
+        Assert.assertEquals("Register", logInPage.registerPageHeadingLocator);
+    }
+
+    @Test
+    public void testInvalidPassword() {
+        homePage.clickOnLogInButton();
+        logInPage.loginFunctionality("ivanans@hotmail.com", "swarovskiPass789");
+
+        Assert.assertEquals("Your email address or password was incorrect.", logInPage.errorMessageLocator);
+    }
+
+
+
+
+}
